620bf6c2d3f8e39ac9a050820cecb989b223253d
Sympa116 <sympa.programming@gmail.com>
2023-07-28 23:39:23 +0900

    Advanced

--- Study/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Robot exRobot = new Robot(); // exRobot「インスタンス（ロボット）」を生成.
-                                     // 設計図からロボットが完成し、実際に使える状態になる.
-        exRobot.color = "青"; // インスタンス変数colorに"青"を代入
-        exRobot.hello(); // helloメソッドを使用
-        exRobot.getColor(); // getColorメソッドを使用
-    }
-}
